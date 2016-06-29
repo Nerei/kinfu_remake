17bf757f11e8299ad478ed069026b9cbec9014bb
Gabriel Cuendet <gabriel.cuendet@epfl.ch>
2016-06-29 10:03:54 +0200

    Add a bit of doc, a constructor to initialize kinfu with custom parameters

--- apps/demo_bin.cpp
+++ apps/demo_bin.cpp
@@ -10,6 +10,13 @@ using namespace kfusion;
 
 struct KinFuApp
 {
+  /**
+   * @name KeyboardCallback
+   * @fn static void KeyboardCallback(const cv::viz::KeyboardEvent& event, void* pthis)
+   * @brief Define keyboard callback for the Kinfu app
+   * @param[in] event
+   * @param[in] pthis, void pointer on itself
+   */
   static void KeyboardCallback(const cv::viz::KeyboardEvent& event, void* pthis)
   {
       KinFuApp& kinfu = *static_cast<KinFuApp*>(pthis);
@@ -21,10 +28,16 @@ struct KinFuApp
           kinfu.take_cloud(*kinfu.kinfu_);
 
       if(event.code == 'i' || event.code == 'I')
-          kinfu.iteractive_mode_ = !kinfu.iteractive_mode_;
+          kinfu.interactive_mode_ = !kinfu.interactive_mode_;
   }
 
-  KinFuApp(BinSource& source) : exit_ (false),  iteractive_mode_(false), capture_ (source), pause_(false) {
+  /**
+   * @name KinFuApp
+   * @fn KinFuApp(BinSource& source)
+   * @brief Constructor of the struct, only taking a BinSource data source
+   * @param[in] source, a BinSource from which the frames are grabbed
+   */
+  KinFuApp(BinSource& source) : exit_ (false),  interactive_mode_(false), capture_ (source), pause_(false) {
       KinFuParams params = KinFuParams::default_params();
       kinfu_ = KinFu::Ptr( new KinFu(params) );
 
@@ -36,6 +49,30 @@ struct KinFuApp
       viz.registerKeyboardCallback(KeyboardCallback, this);
   }
 
+  /**
+   * @name KinFuApp
+   * @fn KinFuApp(BinSource& source, const KinFuParams& params)
+   * @brief Constructor of the struct, allowing to initialize KinFu to different parameters
+   * @param[in] source, a BinSource from which the frames are grabbed
+   * @param[in] params, a KinFuParams struct containing the parameters
+   */
+  KinFuApp(BinSource& source, const KinFuParams& params) : exit_ (false),  interactive_mode_(false), capture_ (source), pause_(false) {
+    kinfu_ = KinFu::Ptr( new KinFu(params) );
+
+    capture_.setRegistration(true);
+
+    cv::viz::WCube cube(cv::Vec3d::all(0), cv::Vec3d(params.volume_size), true, cv::viz::Color::apricot());
+    viz.showWidget("cube", cube, params.volume_pose);
+    viz.showWidget("coor", cv::viz::WCoordinateSystem(0.1));
+    viz.registerKeyboardCallback(KeyboardCallback, this);
+  }
+
+  /**
+   * @name show_depth
+   * @fn void show_depth(const cv::Mat& depth)
+   * @brief Display the depth stream, after normalization
+   * @param[in] depth, the depth image to normalize and display
+   */
   void show_depth(const cv::Mat& depth) {
       cv::Mat display;
       //cv::normalize(depth, display, 0, 255, cv::NORM_MINMAX, CV_8U);
@@ -43,9 +80,15 @@ struct KinFuApp
       cv::imshow("Depth", display);
   }
 
+  /**
+   * @name show_raycasted
+   * @fn void show_raycasted(KinFu& kinfu)
+   * @brief Show the reconstructed scene (using raycasting)
+   * @param[in] kinfu instance
+   */
   void show_raycasted(KinFu& kinfu) {
       const int mode = 3;
-      if (iteractive_mode_)
+      if (interactive_mode_)
           kinfu.renderImage(view_device_, viz.getViewerPose(), mode);
       else
           kinfu.renderImage(view_device_, mode);
@@ -55,6 +98,12 @@ struct KinFuApp
       cv::imshow("Scene", view_host_);
   }
 
+  /**
+   * @name take_cloud
+   * @fn void take_cloud(KinFu& kinfu)
+   * @brief Fetch cloud and display it
+   * @param[in] kinfu instance
+   */
   void take_cloud(KinFu& kinfu)
   {
       cuda::DeviceArray<Point> cloud = kinfu.tsdf().fetchCloud(cloud_buffer);
@@ -64,6 +113,12 @@ struct KinFuApp
       //viz.showWidget("cloud", cv::viz::WPaintedCloud(cloud_host));
   }
 
+  /**
+   * @name execute
+   * @fn bool execute()
+   * @brief Run the main loop of the app
+   * @return true if no error, false otherwise
+   */
   bool execute()
   {
       KinFu& kinfu = *kinfu_;
@@ -90,7 +145,7 @@ struct KinFuApp
           show_depth(depth);
           //cv::imshow("Image", image);
 
-          if (!iteractive_mode_)
+          if (!interactive_mode_)
               viz.setViewerPose(kinfu.getCameraPose());
 
           int key = cv::waitKey(pause_ ? 0 : 3);
@@ -98,7 +153,7 @@ struct KinFuApp
           switch(key)
           {
           case 't': case 'T' : take_cloud(kinfu); break;
-          case 'i': case 'I' : iteractive_mode_ = !iteractive_mode_; break;
+          case 'i': case 'I' : interactive_mode_ = !interactive_mode_; break;
           case 27: exit_ = true; break;
           case 32: pause_ = !pause_; break;
           }
@@ -109,15 +164,26 @@ struct KinFuApp
       return true;
   }
 
-  bool pause_ /*= false*/;
-  bool exit_, iteractive_mode_;
+  /**< */
+  bool pause_; // = false
+  /**< Stop the execution when set to true */
+  bool exit_;
+  /**< Allow for free point of view (otherwise, follows the camera) */
+  bool interactive_mode_;
+  /**< Reference to a source of data BinSource */
   BinSource& capture_;
+  /**< Pointer to the instance of kinfu */
   KinFu::Ptr kinfu_;
+  /**< */
   cv::viz::Viz3d viz;
 
+  /**< View of the scene (raycasting) */
   cv::Mat view_host_;
+  /**< View of the scene on the GPU */
   cuda::Image view_device_;
+  /**< Depth frame on the GPU */
   cuda::Depth depth_device_;
+  /**< */
   cuda::DeviceArray<Point> cloud_buffer;
 };
 
@@ -135,19 +201,14 @@ int main (int argc, char* argv[])
 
   BinSource capture(argv[1], argv[2]);
 
-  //capture.open("d:/onis/20111013-224932.oni");
-  //capture.open("d:/onis/reg20111229-180846.oni");
-  //capture.open("d:/onis/white1.oni");
-  //capture.open("/media/Main/onis/20111013-224932.oni");
-  //capture.open("20111013-225218.oni");
-  //capture.open("d:/onis/20111013-224551.oni");
-  //capture.open("d:/onis/20111013-224719.oni");
+  KinFuParams custom_params = KinFuParams::default_params();
+  custom_params.volume_dims = Vec3i::all(768);
+  custom_params.volume_size = Vec3f::all(0.7f);
+  custom_params.volume_pose = Affine3f().translate(Vec3f(-custom_params.volume_size[0]/2, -custom_params.volume_size[1]/2, 0.5f));
+  custom_params.intr = Intr(520.89, 520.23, 324.54, 237.553);
+  custom_params.tsdf_trunc_dist = 0.005;
 
-  KinFuParams my_params = KinFuParams::default_params();
-  my_params.volume_dims = Vec3i::all(768);
-  my_params.volume_size = Vec3i::all(0.4f);
-
-  KinFuApp app (capture);
+  KinFuApp app (capture, custom_params);
 
   // executing
   try { app.execute (); }
